25be4a47eecd073a637eff4ee25b9ccdc02138c4
Abhijeet <abhijeetsuryawanshi128@gmail.com>
2023-11-23 12:02:44 +0530

    First commit

---
+++ password_generator.py
@@ -0,0 +1,28 @@
+
+from random import shuffle
+import random
+import pyperclip
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+#Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    let = int(input("Enter number of letter you want in your password: "))
+    sym = int(input("Enter number of symbols you want in your password: "))
+    num = int(input("Enter number of numbers you want in your password: "))
+
+    password_letters = [random.choice(letters) for _ in range(let)]
+    password_symbols = [random.choice(symbols) for _ in range(sym)]
+    password_numbers = [random.choice(numbers) for _ in range(num)]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    pyperclip.copy(password)
+    print(password)
+generate_password()
